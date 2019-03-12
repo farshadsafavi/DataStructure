b910748831306498564eb948528c05d019f45579
farshadsafavi <farshadtom2003@gmail.com>
2019-03-12 16:55:52 -0400

    Binary search and Linear search

--- Array/main.c
+++ Array/main.c
@@ -59,6 +59,53 @@ int Delete(struct Array2 *arr, int index){
     }
     return x;
 }
+void swap(int *x, int *y){
+    int temp = *y;
+    *y = *x;
+    *x = temp;
+}
+
+int search(struct Array2 *arr, int x){
+    int i;
+    for(i = 0; i < arr->length; i++){
+        if(arr->A[i] == x){
+            swap(&arr->A[0], &arr->A[i]);
+            return i;
+        }
+    }
+    return -1;
+}
+
+int Bsearch(struct Array2 *arr, int x){
+    int start = 0;
+    int end = arr->length - 1;
+    while( start <= end){
+        int i = (start + end) / 2;
+        printf("%d", i);
+        if(arr->A[i] < x){
+            start = i + 1;
+        }else if (arr->A[i] > x){
+            end = i - 1;
+        }else if (arr->A[i] == x){
+            return i;
+        }
+    }
+    return -1;
+}
+
+int Bsearch_r(struct Array2 *arr, int start, int end, int x){
+    int i = (start + end) / 2;
+    if( start <= end){
+        if(arr->A[i] < x){
+            return Bsearch_r(arr, i + 1, end, x);
+        }else if (arr->A[i] > x){
+            return Bsearch_r(arr, start, i - 1,  x);
+        }else if (arr->A[i] == x){
+            return i;
+        }
+    }
+    return -1;
+}
 
 int main()
 {
@@ -92,5 +139,34 @@ int main()
     printf("\ndeleted: %d\n", Delete(&arr2, 6));
     Display2(arr2);
 
+    printf("\nfind 5 at index: %d\n", search(&arr2, 5));
+    Display2(arr2);
+    printf("\nfind 5 at index: %d\n", search(&arr2, 5));
+    Display2(arr2);
+    printf("\nfind 5 at index: %d\n", search(&arr2, 5));
+    Display2(arr2);
+
+    int x = 5;
+    int y = 4;
+    swap(&x, &y);
+    printf("\nx = %d and y = %d\n", x, y);
+
+    struct Array2 arr3 = {{6,7,8,9,10},20, 5};
+    printf("\nfind 6 at index: %d\n", Bsearch(&arr3, 6));
+    printf("\nfind 7 at index: %d\n", Bsearch(&arr3, 7));
+    printf("\nfind 8 at index: %d\n", Bsearch(&arr3, 8));
+    printf("\nfind 9 at index: %d\n", Bsearch(&arr3, 9));
+    printf("\nfind 10 at index: %d\n", Bsearch(&arr3, 10));
+    printf("\nfind 11 at index: %d\n", Bsearch(&arr3, 11));
+    printf("\nfind 5 at index: %d\n", Bsearch(&arr3, 5));
+    Display2(arr3);
+
+    printf("\nfind 6 at index: %d\n", Bsearch_r(&arr3, 0, arr3.length, 6));
+    printf("\nfind 7 at index: %d\n", Bsearch_r(&arr3, 0, arr3.length, 7));
+    printf("\nfind 8 at index: %d\n", Bsearch_r(&arr3, 0, arr3.length, 8));
+    printf("\nfind 9 at index: %d\n", Bsearch_r(&arr3, 0, arr3.length, 9));
+    printf("\nfind 10 at index: %d\n", Bsearch_r(&arr3, 0, arr3.length, 10));
+    printf("\nfind 11 at index: %d\n", Bsearch_r(&arr3, 0, arr3.length, 11));
+    printf("\nfind 5 at index: %d\n", Bsearch_r(&arr3, 0, arr3.length, 5));
     return 0;
 }
