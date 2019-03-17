399da3335f59a8dc75de34bbe8a4b8a577a5ab89
farshadsafavi <farshadtom2003@gmail.com>
2019-03-17 17:09:12 -0400

    Complete Binary Operations

--- BinaryOperationsComplete/main.c
+++ BinaryOperationsComplete/main.c
@@ -40,15 +40,98 @@ struct array* merge(struct array *arr1, struct array *arr2){
     return arr3;
 };
 
+struct array* Union(struct array *arr1, struct array *arr2){
+    int i = 0, j = 0, k = 0;
+    struct array *arr3=(struct array *)malloc(sizeof(struct array));
+    arr3->length = 0;
+    printf("Start Union!\n");
+    while(i < arr1->length && j < arr2->length){
+        printf("i = %d and j = %d\n", i, j);
+        if(arr1->A[i] < arr2->A[j]){
+            arr3->A[k++] = arr1->A[i++];
+        } else if(arr1->A[i] > arr2->A[j]){
+            arr3->A[k++] = arr2->A[j++];
+        } else{
+            arr3->A[k++] = arr2->A[j++];
+            i++;
+        }
+        arr3->length++;
+    }
+    while(i < arr1->length){
+        printf("i = %d and j = %d\n", i, j);
+        arr3->A[k++] = arr1->A[i++];
+        arr3->length++;
+    }
+
+    while(j < arr2->length){
+        printf("i = %d and j = %d\n", i, j);
+        arr3->A[k++] = arr2->A[j++];
+        arr3->length++;
+    }
+    return arr3;
+};
+
+struct array* Intersection(struct array *arr1, struct array *arr2){
+    int i = 0, j = 0, k = 0;
+    struct array *arr3=(struct array *)malloc(sizeof(struct array));
+    arr3->length = 0;
+    printf("Start Intersection!\n");
+    while(i < arr1->length && j < arr2->length){
+        printf("i = %d and j = %d\n", i, j);
+        if(arr1->A[i] == arr2->A[j]){
+            arr3->A[k] = arr1->A[i];
+            k++;
+        }
+        i++;
+        j++;
+    }
+    arr3->length = k;
+    return arr3;
+};
+
+struct array* Difference(struct array *arr1, struct array *arr2){
+    int i = 0, j = 0, k = 0;
+    struct array *arr3=(struct array *)malloc(sizeof(struct array));
+    arr3->length = 0;
+    printf("Start Difference!\n");
+    while(i < arr1->length && j < arr2->length){
+        printf("i = %d and j = %d\n", i, j);
+        if(arr1->A[i] < arr2->A[j]){
+            arr3->A[k++] = arr1->A[i++];
+        } else if(arr1->A[i] > arr2->A[j]){
+            j++;
+        } else{
+            j++;
+            i++;
+        }
+    }
+    while(i < arr1->length){
+        printf("i = %d and j = %d\n", i, j);
+        arr3->A[k++] = arr1->A[i++];
+    }
+    arr3->length = k;
+    return arr3;
+};
+
+
+
 int main()
 {
     printf("create an object of an array:\n");
-    struct array arr1 = {{3, 8, 16, 20, 22, 25}, 5, 20};
+    struct array arr1 = {{3, 8, 16, 20, 22, 25}, 6, 20};
     Display(&arr1);
-    struct array arr2 = {{4, 10, 12, 22, 23}, 5, 20};
+    struct array arr2 = {{3, 4, 10, 12, 22, 23}, 6, 20};
     Display(&arr2);
     struct array *arr3;
     arr3 = merge(&arr1, &arr2);
+    Display(arr3);
+    arr3 = Union(&arr1, &arr2);
+    Display(arr3);
+    arr3 = Intersection(&arr1, &arr2);
+    Display(arr3);
+    struct array arr4 = {{2, 6, 10, 15,  17, 18, 19, 25}, 8, 20};
+    struct array arr5 = {{3, 6, 7, 15, 17, 20}, 6, 20};
+    arr3 = Difference(&arr4, &arr5);
     Display(arr3);
 
     return 0;
